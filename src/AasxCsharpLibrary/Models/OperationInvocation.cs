1c90866450e0472eca79d7f7febfe24868573329
Raoul Zebisch <raoul.zebisch@uni-a.de>
2024-03-12 16:05:51 +0100

    Operation Invocation model

---
+++ src/AasxCsharpLibrary/Models/OperationInvocation.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace AdminShellNS.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract]
+    public class OperationInvocation
+    {
+        /// <summary>
+        /// Gets or Sets RequestId
+        /// </summary>
+
+        [MaxLength(128)]
+        [DataMember(Name = "requestId")]
+        public string RequestId { get; set; }
+
+        /// <summary>
+        /// Gets or Sets InputVariables
+        /// </summary>
+        
+        [DataMember(Name = "inputVariables")]
+        public List<IOperationVariable> InputVariables { get; set; }
+
+        /// <summary>
+        /// Gets or Sets InoutputVariables
+        /// </summary>
+        
+        [DataMember(Name = "inoutputVariables")]
+        public List<IOperationVariable> InoutputVariables { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Timestamp
+        /// </summary>
+        
+        [DataMember(Name = "timestamp")]
+        public long? Timestamp { get; set; }
+
+        /// <summary>
+        /// Gets or Sets IsAsync
+        /// </summary>
+        
+        [DataMember(Name = "isAsync")]
+        public bool IsAsync { get; set; }
+
+        /// <summary>
+        /// Gets or Sets HandleId
+        /// </summary>
+        
+        [MaxLength(128)]
+        [DataMember(Name = "handleId")]
+        public string HandleId { get; set; }
+    }
+}
+
